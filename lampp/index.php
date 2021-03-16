08e3999a844f7b26bd95d7bde63b425500486c92
Stanley Markman <lowerlabstan@gmail.com>
2021-03-16 00:07:54 -0400

    Added stylesheets

--- lampp/index.php
+++ lampp/index.php
@@ -3,6 +3,8 @@
 <head>
 <meta http-equiv="Content-Type" content="application/x-www-form-urlencoded"/>
 <title>MusicAppDB</title>
+
+<link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
@@ -41,7 +43,7 @@
 
 	  }
 	  else {
-	    $registration_out = "Error- Username invalid! Either username or password are empty, or user already exists.";
+	    $registration_out = "Error- Username invalid! <br>Either username or password are empty<br> or user already exists.";
 	  }
 	}
 
@@ -52,20 +54,20 @@
 
 				$sql_query = "SELECT * FROM ratings WHERE username = ('$s_user')";
 				$result = mysqli_query($conn, $sql_query);
-				$songrow = mysqli_fetch_assoc($result);
-				while ($songrow = mysql_fetch_assoc($result)) {
-    			$songs_out .= $songrow . "\n"
+				//$songrow = mysqli_fetch_assoc($result);
+				while ($songrow = mysqli_fetch_assoc($result)) {
+    			$songs_out .= $songrow["song"] . ": " . $songrow["rating"] . "<br>";
 				}
 
 	}
 
 	$conn->close();
 	?>
-
+	<div class="forms">
 	<h1> Registration </h1>
 	<form method="POST" action="">
-	User: <input type="text" name="username" placeholder="New Username" /><br>
-	Pass: <input type="text" name="password" placeholder="New Password" /><br>
+	<input type="text" name="username" placeholder="New Username" /><br>
+	<input type="text" name="password" placeholder="New Password" /><br>
 	<input type="submit" name="submit" value="Submit"/>
 	<p><?php
 	  if(!empty($registration_out)){
@@ -76,7 +78,7 @@
 
 	<h1> Retrieve Songs </h1>
 	<form method="POST" action="">
-	User: <input type="text" name="raterusername" placeholder="Rater Username" /><br>
+	<input type="text" name="raterusername" placeholder="Rater Username" /><br>
 	<input type="submit" name="submitsong" value="Submit"/>
 
 	<p><?php
@@ -85,6 +87,7 @@
 	  }
 	?></p>
 	</form>
+	</div>
 
 	</body>
 </html>
